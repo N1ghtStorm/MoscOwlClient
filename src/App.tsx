19ebec4cc40af667012e178ecbab766b79f41b2a
Alexandra <alexbushueva27@gmail.com>
2020-06-20 10:18:17 +0300

    "First commit. Created components"

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import css from './App.module.css';
+import Header from './Components/Header';
+import Navbar from './Components/Navbar/Navbar';
+import LeftSideBar from './Components/LeftSideBar';
+import RightSideBar from './Components/RightSideBar';
 
 const App = () => {
   return (
     <div className={css.app}>
+      <Header />
+      <Navbar />
+      <LeftSideBar />
+      <RightSideBar />
     </div>
   );
 }
